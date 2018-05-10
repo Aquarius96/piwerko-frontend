b72bf3b5597e0def8b16cc675c9990c2be5ecc50
Oskyy <oso96@vp.pl>
2018-05-10 13:53:26 +0200

    1st

--- app/pages/MainPage.js
+++ app/pages/MainPage.js
@@ -3,6 +3,7 @@ import React from 'react';
 const MainPage = () =>
     <div>
         <p>Welcome to my main page!</p>
+        <button type="button" className="btn btn-default">Default</button>
     </div>;
 
 export default MainPage;
