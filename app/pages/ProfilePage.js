10d817ef87b7c9069cea33db660ddd684f00558b
Pawel Mastalerz <paw.mastalerz@gmail.com>
2018-05-24 13:44:50 +0200

    Strona profilowa - edycja danych i ulubione piwa - szkic

--- app/pages/ProfilePage.js
+++ app/pages/ProfilePage.js
@@ -1,34 +1,88 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import '../styles/profile-page.scss';
 import '../styles/button.scss';
 
 class ProfilePage extends Component {
     constructor(props) {
-        super(props);
+        super(props)
+
+        this.state = {
+            editing: false
+        }
+
+        this.handleEditProfileData = this.handleEditProfileData.bind(this);
+        this.handleSaveProfileData = this.handleSaveProfileData.bind(this);
     }
+
+    handleEditProfileData(e) {
+        e.preventDefault();
+        this.setState({
+            editing: !this.state.editing
+        });
+    }
+
+    handleSaveProfileData(e) {
+        e.preventDefault();
+        this.setState({
+            editing: !this.state.editing
+        });
+    }
+
     render() {
         return (
-        <div className="profile-page container">
-            <div className="form">
-            <div className="new-form">
-                <div className="wrapper">
-                    <div className="profile-info-form">
-                    <h1>Marcien</h1>
-                    <p>Imię: Marcien</p>
-                    <p>Nazwisko: Zapadka</p>
-                    <p>Data urodzenia: 25.05.1996</p>
-                    <p>E-mail: Marcien@wp.pl</p>
+            <div className="profile-page container">
+                <div className="form">
+                    <div className="new-form">
+                        <div className="wrapper">
+                            <div className="profile-info-form">
+                                {this.state.editing ?
+                                    <div>
+                                        <h1>Marcin</h1>
+                                        <p>Imię: <input type="text" value="Marcin" /> </p>
+                                        <p>Nazwisko: <input type="text" value="Zapadka" /></p>
+                                        <p>Data urodzenia: <input type="text" value="25.05.1996" /></p>
+                                        <p>E-mail: <input type="text" value="Marcin@wp.pl" /></p>
+                                    </div> :
+                                    <div>
+                                        <h1>Marcin</h1>
+                                        <p>Imię: Marcin</p>
+                                        <p>Nazwisko: Zapadka</p>
+                                        <p>Data urodzenia: 25.05.1996</p>
+                                        <p>E-mail: Marcin@wp.pl</p>
+                                    </div>
+                                }
+                            </div>
+                            <div className="image-form">
+                                <div className="sep"></div>
+                                <img src={'https://i.ytimg.com/vi/z5LhNxi1xK8/maxresdefault.jpg'} height="150" />
+                            </div>
+                            <div className="item1">
+                                {this.state.editing ?
+                                    <button className="edytuj-dane" onClick={this.handleSaveProfileData}>Zapisz</button> :
+                                    <button className="edytuj-dane" onClick={this.handleEditProfileData}>Edytuj dane</button>
+                                }
+                            </div>
+                            <div className="item1"><button className="zmien-avatar">Zmień avatar</button></div>
+                        </div>
+
+                        <div className="new-form">
+                            <h1>Moje ulubione piwa</h1>
+
+                            <div className="fav-beer">
+                                <div className="fav-beer-logo">
+                                    <img src="https://drizly-products2.imgix.net/ci-michelob-ultra-244763edf588f5e5.jpeg?auto=format%2Ccompress&fm=jpeg&q=20" height="40" />
+                                </div>
+                                <div className="fav-beer-name"> Kormoran </div>
+                                <div className="fav-beer-desc">
+                                    <div className="fav-beer-alcohol"> Alkohol: 5% </div>
+                                    <div className="fav-beer-IBU"> IBU: 3 </div>
+                                    <div className="fav-beer-rating"> Ocena: 3/5 </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div className="image-form">
-                    <div className="sep"></div>
-                    <img src={'https://i.ytimg.com/vi/z5LhNxi1xK8/maxresdefault.jpg'} height="150"/>
-                    </div>
-                    <div className="item1"><button className="edytuj-dane">Edytuj dane</button></div>
-                    <div className="item1"><button className="zmien-avatar">Zmień avatar</button></div>
-                </div>
                 </div>
             </div>
-        </div>
         );
     }
 }
